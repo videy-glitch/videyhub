3afe5650a19aeaaf4d218f0dccce16d71efda046
videy-glitch <karokeproject@gmail.com>
2026-07-27 13:58:03 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,29 @@
-document.querySelector("button").onclick = function(){
+const loginBtn = document.getElementById("loginBtn");
 
-    window.location.href="dashboard.html";
+loginBtn.addEventListener("click", login);
+
+document.addEventListener("keydown", function(e){
+    if(e.key==="Enter"){
+        login();
+    }
+});
+
+function login(){
+
+    const username=document.getElementById("username").value.trim();
+
+    const password=document.getElementById("password").value.trim();
+
+    if(username==="senpai" && password==="sukajadi1"){
+
+        localStorage.setItem("videyhub_login","true");
+
+        window.location.href="dashboard.html";
+
+    }else{
+
+        document.getElementById("error").textContent="Username atau Password salah.";
+
+    }
 
 }
